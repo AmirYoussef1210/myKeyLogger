17e29f63dc4aa113de3371190861e8144470252f
ay4748 <ay4748@bs.amalnet.k12.il>
2026-03-10 14:58:14 +0200

    for now veery simple code to just print whatever key was pressed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,54 @@
-#include <stdio.h>
+#include <windows.h>
 
-int main()
-{
-	printf("Hello, World!\n");
-	return 0;
+#include <iostream>
+
+
+
+// a function that gets called each time a letter gets pressed
+
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
+
+    if (nCode == HC_ACTION && wParam == WM_KEYDOWN) {
+
+        KBDLLHOOKSTRUCT* pKeyBoard = (KBDLLHOOKSTRUCT*)lParam;
+
+        int key = pKeyBoard->vkCode;
+
+        std::cout << "Key Pressed: " << char(key) << std::endl;
+
+        std::cout << "int of it: " << key << std::endl;
+
+
+
+    }
+
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
+
 }
+
+
+
+int main() {
+
+    HHOOK hhkLowLevelKybd = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, GetModuleHandle(NULL), 0);
+
+
+
+    MSG msg;
+
+    while (GetMessage(&msg, NULL, 0, 0)) {
+
+        TranslateMessage(&msg);
+
+        DispatchMessage(&msg);
+
+    }
+
+
+
+    UnhookWindowsHookEx(hhkLowLevelKybd);
+
+    return 0;
+
+}
+
